56b1c412452e2d91b9ed78c3cb5488f2d49aa324
Neels Moller <xavier.tnc@gmail.com>
2018-01-02 00:55:06 +0200

    FC v0.3.20 - FT - player: Added mouse-look and mouse-attack features! Custom bullet startXYs. Use new 'dt' param in update.

--- src/player.js
+++ src/player.js
@@ -15,7 +15,10 @@ class Player extends Sprite {
 
     this.bullets = [];
     this.lastAttack = 0;
+    this.maxFireRate = 100; // ms per shot
     this.className = 'wizzard';
+
+    this.facingAngle = 0;
 
     FC.lib.extend(this, props);
 
@@ -44,20 +47,17 @@ class Player extends Sprite {
   }
 
 
-  _fireBullet(now) {
+  _fireBullet(now, dt, bulletAngle) {
 
       // Rate and quantity limit attacks...
-      if (this.bullets.length < 10 && (now - this.lastAttack > 100)) {
+      if (this.bullets.length < 10 && (now - this.lastAttack) > this.maxFireRate) {
 
         this.lastAttack = now;
 
-        let bulletW = 16;
-        let bulletH = 16;
+        let bulletX, bulletY;
+        let bulletW = 16, bulletH = 16;
         let bulletHW = bulletW/2;
 
-        let bulletX, bulletY;
-
-        let bulletDir = this.animator ? this.animator.currentAnimation.dir : 0;
         let bulletFacing = this.animator ? this.animator.currentAnimation.facing : 'Right';
 
         switch (bulletFacing)
@@ -84,7 +84,7 @@ class Player extends Sprite {
             break;
           case 'UpRight':
             bulletX = this.x + this.width;
-            bulletY = this.y - bulletH;
+            bulletY = this.y + 4;
             break;
           case 'DownLeft':
             bulletX = this.x;
@@ -92,7 +92,7 @@ class Player extends Sprite {
             break;
           case 'DownRight':
             bulletX = this.x;
-            bulletY = this.y + 16;
+            bulletY = this.y + 8;
             break;
         }
 
@@ -103,7 +103,8 @@ class Player extends Sprite {
             width:16,
             height:16,
             speed:56,
-            dir: bulletDir 
+            facing: bulletFacing,
+            angle: bulletAngle
         });
 
         this.bullets.push(bullet);
@@ -113,16 +114,14 @@ class Player extends Sprite {
   }
 
 
-  _updateBullets(now) {
+  _updateBullets(now, dt) {
 
     let game = FC.game;
 
     // Update LIVE bullets
     this.bullets.forEach(function(bullet) {
 
-      bullet.update(now); // Update mainly position x,y
-
-      bullet.afterUpdate(now);
+      bullet.update(now, dt); // Update mainly position x,y
 
       if (bullet.state === 'Live' && bullet.detectCollision(game.enemy)) {
 
@@ -140,7 +139,7 @@ class Player extends Sprite {
   }
 
 
-  _afterUpdateBullets(now) {
+  _afterUpdateBullets(now, dt) {
 
     // Remove USED bullets  - AND -
     // run "AfterUpdate" on remaining LIVE bullets
@@ -154,7 +153,7 @@ class Player extends Sprite {
       } 
 
       // Keep bullet
-      bullet.afterUpdate(now);
+      bullet.afterUpdate(now, dt);
       return 1;
 
     });
@@ -169,19 +168,9 @@ class Player extends Sprite {
   }
 
 
-  update(now) {
+  update(now, dt) {
 
-    if ( ! this.lastUpdateTime) { this.lastUpdateTime = now; }
-    let dt = now - this.lastUpdateTime; // milliseconds
-    this.lastUpdateTime = now;
-
-    if (dt >= 14) { // Rate limit updates to 14ms per update or max. 71 fps
-
-      if (FC.input.directionChanged) {
-
-        this.animator.currentAnimation = this.animator.getAnimationFacing(FC.input.direction);
-
-      }
+    if (dt > 14) { // Rate limit updates to 14ms per update or max. 71 fps 
 
       if (FC.input.directions.includes("Up"))    { this.y = FC.lib.approach(this.y, 0, this._dy(dt)); }
       if (FC.input.directions.includes("Down"))  { this.y = FC.lib.approach(this.y, FC.view.getHeight() - this.height, this._dy(dt)); }
@@ -189,16 +178,62 @@ class Player extends Sprite {
       if (FC.input.directions.includes("Right")) { this.x = FC.lib.approach(this.x, FC.view.getWidth() - this.width, this._dx(dt)); }
 
 
-      let action = FC.input.action;
+      let mouseAngle = undefined;
 
+      if (FC.input.mode === 'mouse' || (FC.input.mouseMoved && !FC.input.directions.length)) {
 
-      if (action === "Attack") {
+        mouseAngle = FC.lib.getAngle(
+          this.x + this.hw,
+          this.y + this.hh, 
+          FC.input.mouseX - FC.view.getX(),
+          FC.input.mouseY - FC.view.getY()
+        );
 
-        this._fireBullet(now);
+        if (FC.input.mouseMoved) {
+
+          FC.input.mode = 'mouse';
+
+          //console.log('Mouse Moved: mouseAngle =', mouseAngle);
+
+          if (mouseAngle >  345 || mouseAngle < 15  ) { FC.input.direction = 'Right';     } else
+          if (mouseAngle >= 15  && mouseAngle <= 75 ) { FC.input.direction = 'UpRight';   } else
+          if (mouseAngle >  75  && mouseAngle <  105) { FC.input.direction = 'Up';        } else
+          if (mouseAngle >= 105 && mouseAngle <= 165) { FC.input.direction = 'UpLeft';    } else
+          if (mouseAngle >  165 && mouseAngle <  195) { FC.input.direction = 'Left';      } else
+          if (mouseAngle >= 195 && mouseAngle <= 255) { FC.input.direction = 'DownLeft';  } else
+          if (mouseAngle >  255 && mouseAngle <  285) { FC.input.direction = 'Down';      } else
+          if (mouseAngle => 285 && mouseAngle <= 345) { FC.input.direction = 'DownRight'; }
+
+          if (FC.input.direction !== FC.input.lastDirection) { FC.input.directionChanged = true; }
+
+        }
 
       }
 
-      this._updateBullets(now);
+      if (this.animator && FC.input.directionChanged) {
+
+        this.animator.currentAnimation = this.animator.getAnimationFacing(FC.input.direction);
+        this.facingAngle = this.animator.currentAnimation.dir;
+
+      }
+
+      if (FC.input.directionChanged || FC.input.mouseMoved) {
+
+        FC.input.keyDirectionView.innerText = 'Facing: ' + FC.input.direction + 
+          ', ' + this.facingAngle + ', ' + (mouseAngle|0);
+
+      }
+
+
+      let action = FC.input.action;
+
+      if (action === "Attack") {
+
+        this._fireBullet(now, dt, mouseAngle ? mouseAngle : this.facingAngle);
+
+      }
+
+      this._updateBullets(now, dt);
 
     }
 
@@ -207,10 +242,10 @@ class Player extends Sprite {
   } // end: player.update
 
 
-  afterUpdate(now) {
+  afterUpdate(now, dt) {
 
-    this._afterUpdateBullets(now);
-    super.afterUpdate(now);
+    this._afterUpdateBullets(now, dt);
+    super.afterUpdate(now, dt);
 
   }
 
