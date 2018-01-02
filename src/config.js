dbd3fd092adbc1dfe279a5826dbb39555c136f4e
Neels Moller <xavier.tnc@gmail.com>
2018-01-02 16:27:56 +0200

    FC v0.4.3 - DEV - config: Add player attack animations for each facing direction.

--- src/config.js
+++ src/config.js
@@ -15,10 +15,13 @@ FC.config = {
       defaultAnimationId: 0,
 
       animationsCfg: [
+
+        // NORMAL
         {
           id: 0,
           dir: 0,
           speed: 0,
+          state: 'Normal',
           facing: 'Right',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 0 , width: 30, height: 56 }],
@@ -28,6 +31,7 @@ FC.config = {
           id: 1,
           dir: 45,
           speed: 0,
+          state: 'Normal',
           facing: 'UpRight',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 29, width: 29, height: 56 }],
@@ -37,6 +41,7 @@ FC.config = {
           id: 2,
           dir: 90,
           speed: 0,
+          state: 'Normal',
           facing: 'Up',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 60 , width: 30, height: 56 }],
@@ -46,6 +51,7 @@ FC.config = {
           id: 3,
           dir: 135,
           speed: 0,
+          state: 'Normal',
           facing: 'UpLeft',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 29, width: 29, height: 56, flipH: true }],
@@ -55,6 +61,7 @@ FC.config = {
           id: 4,
           dir: 180,
           speed: 0,
+          state: 'Normal',
           facing: 'Left',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 0 , width: 30, height: 56, flipH: true }],
@@ -64,6 +71,7 @@ FC.config = {
           id: 5,
           dir: 225,
           speed: 0,
+          state: 'Normal',
           facing: 'DownLeft',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 127, width: 27, height: 56, flipH: true }],
@@ -73,6 +81,7 @@ FC.config = {
           id: 6,
           dir: 270,
           speed: 0,
+          state: 'Normal',
           facing: 'Down',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 96, width: 30, height: 56 }],
@@ -82,11 +91,94 @@ FC.config = {
           id: 7,
           dir: 315,
           speed: 0,
+          state: 'Normal',
           facing: 'DownRight',
           defaultFrameIndex: 0,
           framesCfg: [{ index: 0, top: 0, left: 127, width: 27, height: 56 }],
           className: 'wizzard'
-        }
+        },
+
+        // ATTACK
+        {
+          id: 8,
+          dir: 0,
+          speed: 0,
+          state: 'Attack',
+          facing: 'Right',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 159, width: 29, height: 56 }],
+          className: 'wizzard'
+        },
+        {
+          id: 9,
+          dir: 45,
+          speed: 0,
+          state: 'Attack',
+          facing: 'UpRight',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 189, width: 33, height: 56 }],
+          className: 'wizzard'
+        },
+        {
+          id: 10,
+          dir: 90,
+          speed: 0,
+          state: 'Attack',
+          facing: 'Up',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 224 , width: 30, height: 56 }],
+          className: 'wizzard'
+        },
+        {
+          id: 11,
+          dir: 135,
+          speed: 0,
+          state: 'Attack',
+          facing: 'UpLeft',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 189, width: 33, height: 56, flipH: true }],
+          className: 'wizzard'
+        },
+        {
+          id: 12,
+          dir: 180,
+          speed: 0,
+          state: 'Attack',
+          facing: 'Left',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 160, width: 29, height: 56, flipH: true }],
+          className: 'wizzard'
+        },
+        {
+          id: 13,
+          dir: 225,
+          speed: 0,
+          state: 'Attack',
+          facing: 'DownLeft',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 288, width: 36, height: 56, flipH: true }],
+          className: 'wizzard'
+        },
+        {
+          id: 14,
+          dir: 270,
+          speed: 0,
+          state: 'Attack',
+          facing: 'Down',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 257, width: 30, height: 56 }],
+          className: 'wizzard'
+        },
+        {
+          id: 15,
+          dir: 315,
+          speed: 0,
+          state: 'Attack',
+          facing: 'DownRight',
+          defaultFrameIndex: 0,
+          framesCfg: [{ index: 0, top: 0, left: 288, width: 36, height: 56 }],
+          className: 'wizzard'
+        }        
       ]
 
     }
