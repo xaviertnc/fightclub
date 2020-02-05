a9871a5828d263d28a8d31173e67aac4801505a7
xaviertnc <xavier.tnc@gmail.com>
2020-02-05 16:32:03 +0200

    FC v0.6.1 - FT - view.js: Now extends the Sprite class. Added this.children, this._appendChildren(), this.addItem(), ...

--- src/view.js
+++ src/view.js
@@ -5,61 +5,134 @@
  * @author: C. Moller
  * @date: 20 December 2017
  *
+ * @updated: 05 Feb 2020 (C. Moller)
+ *   - Now extends Sprite
+ *   - Added this.children = {}
+ *   - Added mount(), _appendChildren(), getChildElement(),
+ *     addItem(), getItem()
+ *   - Set width / height on getWidth(), getHeight()
+ *
  */
 
-class View {
+class View extends Sprite {
 
-  constructor(id) {
 
-    this.id = id || 'main-view';
+  // children = { id1: { elm1: ..., tagName: ... }, id2: { elm2: ..., tagName: ... }, ... }
+  constructor(id, game, type) {
 
-    this.elm = document.getElementById(this.id);
-
-    console.log('View("#' + this.id +'") =', this);
+    super(id, game, type);
+    this.children = {};
 
   }
 
 
-  addElement(childElement, childTagName) {
+  _appendChildren() {
+    this.game.log('View._appendChildren()', this.id + ':', this.children);
+    for (let childID in this.children) {
+      let child = this.children[childID];
+      if ( ! child.elm) {
+        child.elm = document.createElement(child.tagName || 'div');
+        child.elm.id = childID;
+      }
+      // this.game.log('View._appendChildren(), child:', child);
+      this.elm.appendChild(child.elm);
+    }
 
-    if ( ! childElement) {
+  }
 
-      childElement = document.createElement(childTagName || 'div');
+
+  mount(parentElm) {
+
+    if ( ! parentElm) {
+
+      this.elm = document.getElementById(this.id);
+
+      if (this.elm) {
+        this.mounted = true;
+        this._appendChildren();
+        this.game.log('View.mount()', this.type + ': ' + this.id, '- View already mounted!', this.elm);
+        return this;
+      }
+
+      parentElm = document.body;
 
     }
 
-    this.elm.appendChild(childElement);
+    if ( ! this.elm) {
+      this.elm = document.createElement(this.tagName || 'div');
+    }
 
-    return childElement;
+    this._appendChildren();
+
+    parentElm.appendChild(this.elm);
+
+    this.mounted = true;
+
+    this.game.log('View.mount()', this.id, '- Done, parentElm =', parentElm);
+
+    return this;
+
+  }
+
+
+  addItem(itemDef) {
+
+    let child = { elm: null, tagName: 'div' };
+
+    if (typeof itemDef === 'string') {
+      this.children[itemDef] = child;
+    }
+    else {
+      if (itemDef.tagName) { child.tagName = childData.tagName; }
+      if (itemDef.elm) { child.elm = childData.elm; }
+      this.children[itemDef.id] = child;
+    }
+
+    return child;
+
+  }
+
+
+  getItem(itemID) {
+
+    return this.children[itemID];
+
+  }
+
+
+  getChildElement(childID) {
+
+    return this.children[childID].elm;
 
   }
 
 
   getWidth() {
 
-    return this.elm.clientWidth;
+    return this.width = this.elm.clientWidth;
 
   }
 
 
   getHeight() {
 
-    return this.elm.clientHeight;
+    return this.height = this.elm.clientHeight;
 
   }
 
 
   getX() {
 
-    return this.elm.offsetLeft;
+    return this.x = this.elm.offsetLeft;
 
   }
 
 
   getY() {
 
-    return this.elm.offsetTop;
+    return this.y = this.elm.offsetTop;
 
   }
 
-}
+
+}  // End: View Class
